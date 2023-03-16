b882529c85e276b34d94933cbf9a0d2ad35c8dc9
mziaber <magdalena.ziaber@gmail.com>
2023-03-16 14:38:08 +0100

    Add score updating function

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const gameType = document.querySelector('#gameType');
 const score1 = document.querySelector('#score1');
 const score2 = document.querySelector('#score2');
 const reset = document.querySelector('#reset');
-
 const add1 = document.querySelector('#addPlayer1');
-const add2 = document.querySelector('#addPlayer2')
+const add2 = document.querySelector('#addPlayer2');
+const main_div = document.querySelector('#main');
 
 gameInfo.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -24,49 +24,82 @@ gameInfo.addEventListener('submit', (e) => {
 });
 
 reset.addEventListener('click', () => {
-    gameInfo.reset(); 
+    gameInfo.reset();
     score1.innerText = 0;
     score2.innerText = 0;
     name1.innerText = 'Player 1'
     name2.innerText = 'Player 2'
     document.querySelector("#start").disabled = false;
-    remove_num(1);
-    remove_num(2);
+    add1.disabled = true;
+    add2.disabled = true;
+    remove_num();
 });
 
 add1.addEventListener('click', () => {
-    remove_num(2);
-    addScores(1);
+    remove_num();
+    addScores(name1.innerText, 1);
     add1.disabled = true;
     add2.disabled = false;
-    
+
 });
 
+
 add2.addEventListener('click', () => {
-    remove_num(1);
-    addScores(2);
+    remove_num();
+    addScores(name2.innerText, 2);
     add2.disabled = true;
     add1.disabled = false;
 });
 
-function remove_num(n){
-    if (document.querySelector(`#score_row${n}`)){
-        document.querySelector(`#score_row${n}`).remove();
+const formParent = document.querySelector('#main');
+formParent.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const pointsForm = e.target.closest('#main > #score_row > #points');
+    if (pointsForm) {
+        const scoreNum = document.querySelector('#points > input');
+        if (scoreNum.id === 'scoreNum1') {
+            let new_score = parseInt(score1.innerText);
+            update_score(score1, scoreNum, new_score);
+        }
+        if (scoreNum.id === 'scoreNum2') {
+            let new_score = parseInt(score2.innerText);
+            update_score(score2, scoreNum, new_score);
+        }
+        remove_num();
+    }
+});
+
+function remove_num() {
+    if (document.querySelector(`#score_row`)) {
+        document.querySelector(`#score_row`).remove();
     }
 }
 
-function addScores(n){
+function addScores(name, n) {
     newRow = document.createElement('row');
-    newRow.setAttribute("id", `score_row${n}`);
-    newRow.innerHTML = "<div class='col py-2' ><form><span>Player "+ n +" scores: </span><input class='form-control w-25 m-auto my-2' id='scoreNum" + n +"'type='number'></input><button class='btn btn-secondary' id='addScores'>Submit</button></form></div>";
+    newRow.setAttribute("id", `score_row`);
+    newRow.innerHTML = "<form id='points'><span>" + name + " scores: </span><input class='form-control w-25 m-auto my-2' id='scoreNum" + n + "'type='number'></input><button class='btn btn-secondary'>Submit</button></form>";
     document.querySelector("#main").appendChild(newRow);
 }
 
 
-function check(name,playerName,n) {
-    if (name != ''){
+function check(name, playerName, n) {
+    if (name != '') {
         playerName.innerText = name;
     } else {
         playerName.innerText = `Player ${n}`
+    }
+}
+
+function update_score(score, score_num, new_score) {
+    new_score -= score_num.valueAsNumber;
+    if (new_score > 0 && new_score != 1) {
+        score.innerText = new_score;
+    } else if (new_score === 0) {
+        alert('End of game')
+        document.querySelector("#reset").click()
+    } else {
+        alert('To much points!');
+
     }
 }
